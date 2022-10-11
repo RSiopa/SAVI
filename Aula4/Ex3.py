6102580c906043813393d3eca8e58ef440543e9a
RSiopa <siopainacio98@ua.pt>
2022-10-11 17:10:52 +0100

    Aula 4 Bugs

--- Aula4/Ex3.py
+++ Aula4/Ex3.py
@@ -26,28 +26,12 @@ def main():
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
     cv2.resizeWindow(window_name, 800, 500)
 
-    number_of_people = 0
-
-    csv_reader = csv.reader(open('TownCentre-groundtruth.top'))
-    for row in csv_reader:
-
-        if len(row) != 12:
-            continue
-
-        personNumber, _, _, _, _, _, _, _, _, _, _, _ = row
-        personNumber = int(personNumber)
-
-        if personNumber >= number_of_people:
-            number_of_people = personNumber + 1
-
-    colors = np.random.randint(0, high=255, size=(number_of_people, 3), dtype=int)
-
-    person_detector = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+    person_detector = cv2.CascadeClassifier('../Aula4/haarcascade_fullbody.xml')
 
     detection_counter = 0
     tracker_counter = 0
     trackers = []
-    iou_threshold = 128
+    iou_threshold = 0.8
 
     # -----------------------------------------------------
     # Execution
@@ -64,21 +48,19 @@ def main():
             cv2.waitKey(0)
             break
 
-        image_gray = cv2.cvtColor(image_gui, cv2.COLOR_BGR2GRAY)
+        image_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        bboxes = person_detector.detectMultiscale(image_gray, scaleFactor = 1.05, minNeighbor = 5, minSize = (20, 40))
+        bboxes = person_detector.detectMultiScale(image_gray, scaleFactor = 1.2, minNeighbors = 4, minSize = (20, 40))
 
         detections = []
 
         for bbox in bboxes:
             x1, y1, w, h = bbox
-            detection = Detection(x1, y1, w, h, image_gray)
+            detection = Detection(x1, y1, w, h, image_gray, id = detection_counter)
             detection_counter += 1
             detection.draw(image_gui)
             detections.append(detection)
-            cv2.imshow('detection ' + str(detection.id), detection.image)
-
-            # cv2.rectangle(image_gui, (x1, y1), (x1 + w, y1 + h), (0, 0, 255, 3))
+            # cv2.imshow('detection ' + str(detection.id), detection.image)
 
         for detection in detections:
             for tracker in trackers:
@@ -88,10 +70,11 @@ def main():
                 if iou > iou_threshold:
                     tracker.addDetection(detection)
 
-        for detection in detections:
-            tracker = Tracker(detection, id=tracker_counter)
-            tracker_counter += 1
-            trackers.append(tracker)
+        if frame_counter == 0:
+            for detection in detections:
+                tracker = Tracker(detection, id=tracker_counter)
+                tracker_counter += 1
+                trackers.append(tracker)
 
         for tracker in trackers:
             tracker.draw(image_gui)
--- Aula4/functions.py
+++ Aula4/functions.py
@@ -3,7 +3,7 @@ import cv2
 
 class BoundingBox:
 
-    def __init__(self, x1, y1, w, h, area):
+    def __init__(self, x1, y1, w, h):
         self.x1 = x1
         self.y1 = y1
         self.w = w
@@ -33,9 +33,8 @@ class Detection(BoundingBox):
 
     def __init__(self, x1, y1, w, h, image_full, id):
         super().__init__(x1, y1, w, h)
-
+        self.id = id
         self.extractSmallImage(image_full)
-
 
     def extractSmallImage(self, image_full):
         self.image = image_full[self.y1:self.y1+self.h, self.x1:self.x1+self.w]
@@ -43,21 +42,21 @@ class Detection(BoundingBox):
     def draw(self, image_gui, color=(255, 0, 0)):
         cv2.rectangle(image_gui, (self.x1, self.y1), (self.x2, self.y2), color, 3)
 
-        image = cv2.putText(image_gui, 'D' + str(self.id), (self.x1, self.y1 - 5), cv2.FONT_HERSHEY_SIM, 1, color, 2, cv2.LINE_AA)
+        image = cv2.putText(image_gui, 'D' + str(self.id), (self.x1, self.y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv2.LINE_AA)
 
 
-class Tracker():
+class Tracker:
 
     def __init__(self, detection, id):
         self.detections = [detection]
         self.id = id
 
     def draw(self, image_gui, color=(255, 0, 255)):
-        last_detection = self.detection[-1]
+        last_detection = self.detections[-1]
 
         cv2.rectangle(image_gui, (last_detection.x1, last_detection.y1), (last_detection.x2, last_detection.y2), color, 3)
 
-        image = cv2.putText(image_gui, 'T' + str(self.id), (last_detection.x1 - 40, last_detection.y1 - 5), cv2.FONT_HERSHEY_SIM, 1, color, 2, cv2.LINE_AA)
+        image = cv2.putText(image_gui, 'T' + str(self.id), (last_detection.x2 - 40, last_detection.y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv2.LINE_AA)
 
     def addDetection(self, detection):
         self.detections.append(detection)
@@ -66,4 +65,5 @@ class Tracker():
         text = 'T' + str(self.id) + ' Detections = ['
         for detection in self.detections:
             text += str(detection.id) + ' ,'
-            return text
+
+        return text
